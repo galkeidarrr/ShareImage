da70a00b4f57552a969705d2b8d6ea923a5fe64c
galkeidarrr <39056499+galkeidarrr@users.noreply.github.com>
2019-09-07 15:42:06 +0300

    post details

--- app/src/main/java/com/example/shareimage/Fragments/CommentsFragment.java
+++ app/src/main/java/com/example/shareimage/Fragments/CommentsFragment.java
@@ -1,4 +1,4 @@
-package com.example.shareimage;
+package com.example.shareimage.Fragments;
 
 
 import android.os.Bundle;
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.shareimage.R;
 
 
 /**
